e350071407e98368ee68706ed27dca4fe873ecac
KOZO HIRATA <hirata.k@shanon.co.jp>
2016-12-03 06:56:21 +0900

    add Edge

---
+++ src/test/java/sel3sample/EdgeSample.java
@@ -0,0 +1,58 @@
+package sel3sample;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class EdgeSample {
+
+	WebDriver driver;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		//MicrosoftWebDriver.exeのPATHをOSのPATHに含めない場合はここを記述
+		//System.setProperty("webdriver.edge.driver","MicrosoftWebDriver.exeのPATH");
+		driver = new EdgeDriver();			
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.close();
+		driver.quit();
+	}
+
+	
+	@Test
+	public void test() throws InterruptedException {				
+		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
+		
+		WebElement element = driver.findElement(By.id("lst-ib"));
+		element.sendKeys("シャノン");
+		element.submit();
+		
+		WebDriverWait wait = new WebDriverWait(driver, 10, 10000);
+		wait.until(ExpectedConditions.titleContains("シャノン"));
+		assertThat(driver.getTitle(),is("シャノン - Google 検索"));
+	}
+
+}
